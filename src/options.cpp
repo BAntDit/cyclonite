542e40e6ef6b0ebdc390444447af711789d17b1d
BAntDit <bantdit@gmail.com>
2019-09-09 23:53:51 +0300

    engine options update

--- src/options.cpp
+++ src/options.cpp
@@ -3,18 +3,29 @@
 //
 
 #include "options.h"
-#include <boost/program_options.hpp>
+#include <iostream>
 
 namespace po = boost::program_options;
+namespace pt = boost::property_tree;
 
 namespace cyclonite {
-Options::Options(int argc, const char** argv)
+Options::Options(int argc /* = 0*/, const char* argv[] /* = {}*/)
   : config_{ "config.json" }
+  , deviceName_{ "" }
+  , properties_{}
+  , windows_{}
 {
     po::options_description commandLineOptions{ "commandLineOptions" };
 
     commandLineOptions.add_options()("help, h", "show help")(
-      "config", po::value<std::string>()->default_value(config_), "config file");
+      "config", po::value<std::string>()->default_value(config_), "config file")(
+      "device-name", po::value<std::string>(), "device name to use")(
+      "no-window", po::value<bool>()->default_value(false), "true to prevent default window creation")(
+      "full-screen",
+      po::value<bool>()->default_value(false),
+      "whether default window is in full screen mode (true / false)")(
+      "width", po::value<uint16_t>()->default_value(512), "default window screen width")(
+      "height", po::value<uint16_t>()->default_value(512), "default window screen height");
 
     po::variables_map variables;
 
@@ -26,6 +37,68 @@ Options::Options(int argc, const char** argv)
         config_ = variables["config"].as<std::string>();
     }
 
+    if (variables.count("device-name")) {
+        deviceName_ = variables["device-name"].as<std::string>();
+    }
+
     po::notify(variables);
+
+    if (variables.count("help")) {
+        std::cout << commandLineOptions << std::endl;
+    }
+
+    {
+        std::ifstream ifs(config_);
+
+        if (ifs) {
+            pt::read_json(ifs, properties_);
+        }
+    }
+
+    if (deviceName_.empty() && properties_.count("device")) {
+        deviceName_ = properties_.get<std::string>("device");
+    }
+
+    parseWindowProperties();
+
+    bool changesInWindowProperties = false;
+
+    if (windows_.empty() && !variables["no-window"].as<bool>()) {
+        WindowProperties wp = {};
+
+        wp.title = "Cyclonite";
+        wp.left = 0x1FFF0000;
+        wp.top = 0x1FFF0000;
+        wp.width = variables["width"].as<uint16_t>();
+        wp.height = variables["height"].as<uint16_t>();
+        wp.fullscreen = variables["full-screen"].as<bool>();
+
+        windows_.push_back(wp);
+
+        changesInWindowProperties = true;
+    }
+
+    if (changesInWindowProperties) {
+        // todo:: ...
+    }
+}
+
+void Options::parseWindowProperties()
+{
+    if (properties_.count("windows") == 0)
+        return;
+
+    for (auto const& windowProperties : properties_.get_child("windows")) {
+        WindowProperties wp = {};
+
+        wp.title = windowProperties.second.get<std::string>("title");
+        wp.left = windowProperties.second.get<uint16_t>("left");
+        wp.top = windowProperties.second.get<uint16_t>("top");
+        wp.width = windowProperties.second.get<uint16_t>("width");
+        wp.height = windowProperties.second.get<uint16_t>("height");
+        wp.fullscreen = windowProperties.second.get<bool>("fullscreen");
+
+        windows_.push_back(wp);
+    }
 }
 }
--- src/options.h
+++ src/options.h
@@ -5,16 +5,38 @@
 #ifndef CYCLONITE_OPTIONS_H
 #define CYCLONITE_OPTIONS_H
 
+#include <boost/foreach.hpp>
+#include <boost/program_options.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <cstdint>
 #include <string>
+#include <vector>
 
 namespace cyclonite {
 class Options
 {
 public:
+    struct WindowProperties
+    {
+        std::string title;
+        uint32_t left;
+        uint32_t top;
+        uint32_t width;
+        uint32_t height;
+        bool fullscreen;
+    };
+
     explicit Options(int argc = 0, const char* argv[] = {});
 
 private:
+    void parseWindowProperties();
+
+private:
     std::string config_;
+    std::string deviceName_;
+    boost::property_tree::ptree properties_;
+    std::vector<WindowProperties> windows_;
 };
 }
 
