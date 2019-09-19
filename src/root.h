0199ea41d27ba0c0bd5b5e7b1f3279b46aa2bf32
BAntDit <bantdit@gmail.com>
2019-09-19 11:53:27 +0300

    android surface support

--- src/root.h
+++ src/root.h
@@ -82,6 +82,11 @@ void Root<Config<PlatformConfig, EcsConfig>>::init(Options const& options)
                                          std::array<char const*, 3>{ VK_EXT_DEBUG_REPORT_EXTENSION_NAME,
                                                                      VK_KHR_SURFACE_EXTENSION_NAME,
                                                                      VK_KHR_WIN32_SURFACE_EXTENSION_NAME });
+#elif defined(VK_USE_PLATFORM_ANDROID_KHR)
+      std::make_unique<vulkan::Instance>(std::array<char const*, 1>{ "VK_LAYER_LUNARG_standard_validation" },
+                                         std::array<char const*, 3>{ VK_EXT_DEBUG_REPORT_EXTENSION_NAME,
+                                                                     VK_KHR_SURFACE_EXTENSION_NAME,
+                                                                     VK_KHR_ANDROID_SURFACE_EXTENSION_NAME });
 #else
       std::make_unique<vulkan::Instance>(std::array<char const*, 1>{ "VK_LAYER_LUNARG_standard_validation" },
                                          std::array<char const*, 1>{ VK_EXT_DEBUG_REPORT_EXTENSION_NAME });
@@ -96,6 +101,8 @@ void Root<Config<PlatformConfig, EcsConfig>>::init(Options const& options)
         surfaces_.emplace_back(vulkanInstance_->handle(), window, easy_mp::type_list<wl_display*, wl_surface*>{});
 #elif defined(VK_USE_PLATFORM_WIN32_KHR)
         surfaces_.emplace_back(vulkanInstance_->handle(), window, easy_mp::type_list<HINSTANCE, HWND>{});
+#elif defined(VK_USE_PLATFORM_ANDROID_KHR)
+        surfaces_.emplace_back(vulkanInstance_->handle(), window, easy_mp::type_list<ANativeWindow*>{});
 #endif
     }
 
