529e5acbc8893e12a9f1bc5804771775c015c5f4
BAntDit <bantdit@gmail.com>
2019-09-19 10:36:59 +0300

    added wayland surface

---
+++ src/vulkan/wlSurface.cpp
@@ -0,0 +1,32 @@
+//
+// Created by bantdit on 9/19/19.
+//
+
+#include "wlSurface.h"
+
+#if defined(VK_USE_PLATFORM_WAYLAND_KHR)
+namespace cyclonite::vulkan {
+WlSurface::WlSurface(VkInstance vkInstance, wl_display* display, wl_surface* surface)
+  : vkSurfaceKHR_{ vkInstance, vkDestroySurfaceKHR }
+{
+    VkWaylandSurfaceCreateInfoKHR vkWaylandSurfaceCreateInfoKHR = {};
+
+    vkWaylandSurfaceCreateInfoKHR.sType = VK_STRUCTURE_TYPE_WAYLAND_SURFACE_CREATE_INFO_KHR;
+    vkWaylandSurfaceCreateInfoKHR.display = display;
+    vkWaylandSurfaceCreateInfoKHR.surface = surface;
+
+    auto result = vkCreateWaylandSurfaceKHR(vkInstance, &vkWaylandSurfaceCreateInfoKHR, nullptr, &vkSurfaceKHR_);
+
+    if (result != VK_SUCCESS) {
+        if (result == VK_ERROR_OUT_OF_DEVICE_MEMORY) {
+            throw std::runtime_error("there is no enough memory on device to create wayland surface");
+        }
+
+        if (result == VK_ERROR_OUT_OF_HOST_MEMORY) {
+            throw std::runtime_error("there is no enough memory on host to create wayland surface");
+        }
+    }
+}
+}
+
+#endif
---
+++ src/vulkan/wlSurface.h
@@ -0,0 +1,37 @@
+//
+// Created by bantdit on 9/19/19.
+//
+
+#ifndef CYCLONITE_WLSURFACE_H
+#define CYCLONITE_WLSURFACE_H
+
+#include "platform.h"
+
+#if defined(VK_USE_PLATFORM_WAYLAND_KHR)
+
+namespace cyclonite::vulkan {
+class WlSurface
+{
+public:
+    WlSurface(VkInstance vkInstance, wl_display* display, wl_surface* surface);
+
+    WlSurface(WlSurface const&) = delete;
+
+    WlSurface(WlSurface&&) = default;
+
+    auto operator=(WlSurface const&) -> WlSurface& = delete;
+
+    auto operator=(WlSurface &&) -> WlSurface& = default;
+
+    ~WlSurface() = default;
+
+    [[nodiscard]] auto handle() const -> VkSurfaceKHR { return static_cast<VkSurfaceKHR>(vkSurfaceKHR_); }
+
+private:
+    Handle<VkSurfaceKHR> vkSurfaceKHR_;
+};
+}
+
+#endif
+
+#endif // CYCLONITE_WLSURFACE_H
