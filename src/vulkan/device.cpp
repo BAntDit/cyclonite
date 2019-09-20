1f2022be0710f1b3f33c30f5c5f4bebada8e179d
BAntDit <bantdit@gmail.com>
2019-09-20 11:06:02 +0300

    device update

--- src/vulkan/device.cpp
+++ src/vulkan/device.cpp
@@ -6,63 +6,34 @@
 #include "internal/deviceCreationFunctions.h"
 
 namespace cyclonite::vulkan {
-Device::Device(VkPhysicalDevice const& vkPhysicalDevice)
-  : name_{ "" }
-  , graphicsQueueIndex_{ std::numeric_limits<size_t>::max() }
-  , computeQueueIndex_{ std::numeric_limits<size_t>::max() }
-  , deviceHostTransferIndex_{ std::numeric_limits<size_t>::max() }
-  , presentationQueueIndex_{ std::numeric_limits<size_t>::max() }
-  , vkPhysicalDevice_{ vkPhysicalDevice }
-  , vkDeviceProperties_{}
-  , vkPhysicalDeviceMemoryProperties_{}
-  , vkDevice_{ vkDestroyDevice }
+auto Device::_extractPhysicalDeviceNameFromProperties(VkPhysicalDeviceProperties const& deviceProperties) -> std::string
 {
-    vkGetPhysicalDeviceProperties(vkPhysicalDevice_, &vkDeviceProperties_);
-    vkGetPhysicalDeviceMemoryProperties(vkPhysicalDevice_, &vkPhysicalDeviceMemoryProperties_);
+    return internal::getDeviceName(deviceProperties);
+}
 
-    name_ = internal::getDeviceName(vkDeviceProperties_);
+auto Device::_findBestGraphicsQueueFamilyIndex(std::vector<VkQueueFamilyProperties> const& familyProperties) -> uint32_t
+{
+    return internal::getBestQueueFamilyIndex(familyProperties, VK_QUEUE_GRAPHICS_BIT | VK_QUEUE_TRANSFER_BIT);
+}
 
-    uint32_t familyCount = 0;
+auto Device::_findBestComputeQueueFamilyIndex(std::vector<VkQueueFamilyProperties> const& familyProperties,
+                                              uint32_t graphicsQueueFamilyIndex) -> uint32_t
+{
+    return internal::testQueueCapability(familyProperties, graphicsQueueFamilyIndex, VK_QUEUE_COMPUTE_BIT)
+             ? graphicsQueueFamilyIndex
+             : internal::getBestQueueFamilyIndex(familyProperties, VK_QUEUE_COMPUTE_BIT, 1u);
+}
 
-    vkGetPhysicalDeviceQueueFamilyProperties(vkPhysicalDevice_, &familyCount, nullptr);
-
-    std::vector<VkQueueFamilyProperties> familyPropertiesList(familyCount);
-
-    vkGetPhysicalDeviceQueueFamilyProperties(vkPhysicalDevice_, &familyCount, familyPropertiesList.data());
-
-    uint32_t graphicsQueueFamilyIndex =
-      internal::getBestQueueFamilyIndex(familyPropertiesList, VK_QUEUE_GRAPHICS_BIT | VK_QUEUE_TRANSFER_BIT);
-
-    if (graphicsQueueFamilyIndex == std::numeric_limits<uint32_t>::max()) {
-        throw std::runtime_error("could not found valid graphics queue family index for device: " + name_);
-    }
-
-    uint32_t computeQueueFamilyIndex =
-      internal::testQueueCapability(familyPropertiesList, graphicsQueueFamilyIndex, VK_QUEUE_COMPUTE_BIT)
-        ? graphicsQueueFamilyIndex
-        : internal::getBestQueueFamilyIndex(familyPropertiesList, VK_QUEUE_COMPUTE_BIT, 1u);
-
-    if (computeQueueFamilyIndex == std::numeric_limits<uint32_t>::max()) {
-        throw std::runtime_error("could not found valid compute queue family index for device: " + name_);
-    }
-
+auto Device::_findBestHostTransferQueueFamilyIndex(std::vector<VkQueueFamilyProperties> const& familyProperties,
+                                                   uint32_t graphicsQueueFamilyIndex) -> uint32_t
+{
     uint32_t deviceHostTransferQueueFamilyIndex =
-      internal::getBestQueueFamilyIndex(familyPropertiesList, VK_QUEUE_TRANSFER_BIT, 1u);
+      internal::getBestQueueFamilyIndex(familyProperties, VK_QUEUE_TRANSFER_BIT, 1u);
 
     if (deviceHostTransferQueueFamilyIndex == std::numeric_limits<uint32_t>::max()) {
         deviceHostTransferQueueFamilyIndex = graphicsQueueFamilyIndex;
     }
 
-    // todo:: presentation queue family index
-
-    VkPhysicalDeviceFeatures features = {};
-
-    vkGetPhysicalDeviceFeatures(vkPhysicalDevice_, &features);
-
-    // turn off unused features (for now)
-    features.robustBufferAccess = VK_FALSE;
-    features.shaderFloat64 = VK_FALSE;
-    features.shaderInt64 = VK_FALSE;
-    features.inheritedQueries = VK_FALSE;
+    return deviceHostTransferQueueFamilyIndex;
 }
 }
--- src/vulkan/device.h
+++ src/vulkan/device.h
@@ -11,7 +11,8 @@ namespace cyclonite::vulkan {
 class Device
 {
 public:
-    explicit Device(VkPhysicalDevice const& vkPhysicalDevice);
+    template<typename SurfaceType, template<typename> class Surface>
+    explicit Device(VkPhysicalDevice const& vkPhysicalDevice, std::vector<Surface<SurfaceType>> const& surfaces);
 
     Device(Device const&) = delete;
 
@@ -28,16 +29,80 @@ public:
     [[nodiscard]] auto handle() const -> VkDevice { return static_cast<VkDevice>(vkDevice_); }
 
 private:
+    static auto _extractPhysicalDeviceNameFromProperties(VkPhysicalDeviceProperties const& deviceProperties)
+      -> std::string;
+
+    static auto _findBestGraphicsQueueFamilyIndex(std::vector<VkQueueFamilyProperties> const& familyProperties)
+      -> uint32_t;
+
+    static auto _findBestComputeQueueFamilyIndex(std::vector<VkQueueFamilyProperties> const& familyProperties,
+                                                 uint32_t graphicsQueueFamilyIndex) -> uint32_t;
+
+    static auto _findBestHostTransferQueueFamilyIndex(std::vector<VkQueueFamilyProperties> const& familyProperties,
+                                                      uint32_t graphicsQueueFamilyIndex) -> uint32_t;
+
+private:
+    VkPhysicalDevice vkPhysicalDevice_;
+    VkPhysicalDeviceProperties vkDeviceProperties_;
+    VkPhysicalDeviceMemoryProperties vkPhysicalDeviceMemoryProperties_;
     std::string name_;
     size_t graphicsQueueIndex_;
     size_t computeQueueIndex_;
     size_t deviceHostTransferIndex_;
     size_t presentationQueueIndex_;
-    VkPhysicalDevice vkPhysicalDevice_;
-    VkPhysicalDeviceProperties vkDeviceProperties_;
-    VkPhysicalDeviceMemoryProperties vkPhysicalDeviceMemoryProperties_;
     Handle<VkDevice> vkDevice_;
 };
+
+template<typename SurfaceType, template<typename> class Surface>
+Device::Device(VkPhysicalDevice const& vkPhysicalDevice, std::vector<Surface<SurfaceType>> const& surfaces)
+  : vkPhysicalDevice_{ vkPhysicalDevice }
+  , vkDeviceProperties_{}
+  , vkPhysicalDeviceMemoryProperties_{}
+  , name_{ "" }
+  , graphicsQueueIndex_{ std::numeric_limits<size_t>::max() }
+  , computeQueueIndex_{ std::numeric_limits<size_t>::max() }
+  , deviceHostTransferIndex_{ std::numeric_limits<size_t>::max() }
+  , presentationQueueIndex_{ std::numeric_limits<size_t>::max() }
+  , vkDevice_{ vkDestroyDevice }
+{
+    vkGetPhysicalDeviceProperties(vkPhysicalDevice_, &vkDeviceProperties_);
+    vkGetPhysicalDeviceMemoryProperties(vkPhysicalDevice_, &vkPhysicalDeviceMemoryProperties_);
+
+    name_ = _extractPhysicalDeviceNameFromProperties(vkDeviceProperties_);
+
+    uint32_t familyCount = 0;
+
+    vkGetPhysicalDeviceQueueFamilyProperties(vkPhysicalDevice_, &familyCount, nullptr);
+
+    std::vector<VkQueueFamilyProperties> familyPropertiesList(familyCount);
+
+    vkGetPhysicalDeviceQueueFamilyProperties(vkPhysicalDevice_, &familyCount, familyPropertiesList.data());
+
+    uint32_t graphicsQueueFamilyIndex = _findBestGraphicsQueueFamilyIndex(familyPropertiesList);
+
+    if (graphicsQueueFamilyIndex == std::numeric_limits<uint32_t>::max()) {
+        throw std::runtime_error("could not found valid graphics queue family index for device: " + name_);
+    }
+
+    uint32_t computeQueueFamilyIndex = _findBestComputeQueueFamilyIndex(familyPropertiesList, graphicsQueueFamilyIndex);
+
+    if (computeQueueFamilyIndex == std::numeric_limits<uint32_t>::max()) {
+        throw std::runtime_error("could not found valid compute queue family index for device: " + name_);
+    }
+
+    uint32_t deviceHostTransferQueueFamilyIndex =
+      _findBestHostTransferQueueFamilyIndex(familyPropertiesList, graphicsQueueFamilyIndex);
+
+    VkPhysicalDeviceFeatures features = {};
+
+    vkGetPhysicalDeviceFeatures(vkPhysicalDevice_, &features);
+
+    // turn off unused features (for now)
+    features.robustBufferAccess = VK_FALSE;
+    features.shaderFloat64 = VK_FALSE;
+    features.shaderInt64 = VK_FALSE;
+    features.inheritedQueries = VK_FALSE;
+}
 }
 
 #endif // CYCLONITE_DEVICE_H
