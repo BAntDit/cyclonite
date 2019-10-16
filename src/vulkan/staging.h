0f7c7120a0889d9b3c8ba3194e296108cbcde676
BAntDit <bantdit@gmail.com>
2019-10-16 21:32:27 +0300

    staging class

---
+++ src/vulkan/staging.h
@@ -0,0 +1,35 @@
+//
+// Created by bantdit on 10/16/19.
+//
+
+#ifndef CYCLONITE_STAGING_H
+#define CYCLONITE_STAGING_H
+
+#include "../arena.h"
+#include "buffer.h"
+
+namespace cyclonite::vulkan {
+// TODO:: implement
+class Staging: public Arena<Staging>
+{
+public:
+    Staging(Device& device, VkBufferUsageFlags usageFlags, VkDeviceSize size);
+
+    Staging(Staging const&) = delete;
+
+    Staging(Staging&&) = default;
+
+    ~Staging() = default;
+
+    auto operator=(Staging const&) -> Staging& = delete;
+
+    auto operator=(Staging&&) -> Staging& = default;
+
+    [[nodiscard]] auto ptr() const -> void*;
+
+private:
+    Buffer buffer_;
+};
+}
+
+#endif // CYCLONITE_STAGING_H
