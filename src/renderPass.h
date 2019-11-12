9a38f8881c027705b9099337a8809a59f4b3eda1
BAntDit <bantdit@gmail.com>
2019-11-12 17:32:35 +0200

    renderPass update

--- src/renderPass.h
+++ src/renderPass.h
@@ -5,9 +5,21 @@
 #ifndef CYCLONITE_RENDERPASS_H
 #define CYCLONITE_RENDERPASS_H
 
+#include <vector>
+#include <vulkan/vulkan.h>
+
 namespace cyclonite {
 class RenderPass
-{};
+{
+public:
+    [[nodiscard]] auto renderQueueSubmitInfo() const -> std::vector<VkSubmitInfo> const&
+    {
+        return renderQueueSubmitInfo_;
+    }
+
+private:
+    std::vector<VkSubmitInfo> renderQueueSubmitInfo_;
+};
 }
 
 #endif // CYCLONITE_RENDERPASS_H
