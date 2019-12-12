0290a54de51cf09f33a884acce6d588b01d90678
BAntDit <bantdit@gmail.com>
2019-12-12 18:18:53 +0200

    render target output properties

--- src/renderTarget.h
+++ src/renderTarget.h
@@ -8,10 +8,55 @@
 #include "surface.h"
 
 namespace cyclonite {
-// TODO:: adds enum class outputs
-
 class RenderTarget
 {
+public:
+    enum class RenderTargetOutputSemantic
+    {
+        UNDEFINED = 0,
+        DEFAULT = 1,
+        LINEAR_HDR_COLOR = 2,
+        MIN_VALUE = UNDEFINED,
+        MAX_VALUE = LINEAR_HDR_COLOR,
+        COUNT = MAX_VALUE + 1
+    };
+
+    enum class RenderTargetOutputLayout
+    {
+        UNDEFINED = 0,
+        COLOR_ATTACHMENT = 1,
+        DEPTH_ATTACHMENT = 2,
+        DEPTH_STENCIL_ATTACHMENT = 3,
+        MIN_VALUE = UNDEFINED,
+        MAX_VALUE = DEPTH_STENCIL_ATTACHMENT,
+        COUNT = MAX_VALUE + 1
+    };
+
+    template<VkFormat vkFormat, VkColorSpaceKHR vkColorSpaceKHR>
+    struct output_format_candidate_t
+    {
+        constexpr static VkFormat format = vkFormat;
+        constexpr static VkColorSpaceKHR colorSpace = vkColorSpaceKHR;
+    };
+
+    template<typename Properties, RenderTargetOutputSemantic Semantic, RenderTargetOutputLayout Layout>
+    struct output_t;
+
+    template<RenderTargetOutputSemantic Semantic,
+             RenderTargetOutputLayout Layout,
+             VkFormat... format,
+             VkColorSpaceKHR... colorSpace>
+    struct output_t<easy_mp::type_list<output_format_candidate_t<format, colorSpace>...>, Semantic, Layout>
+    {
+        constexpr static std::array<std::pair<VkFormat, VkColorSpaceKHR>, sizeof...(format)> format_candidate_list_v = {
+            std::make_pair(format, colorSpace)...
+        };
+
+        constexpr static RenderTargetOutputSemantic semantic_v = Semantic;
+
+        constexpr static RenderTargetOutputLayout layout_v = Layout;
+    };
+
 public:
     // template<typename DepthStencilAttachment, typename... ColorAttachments>
     RenderTarget(VkRenderPass vkRenderPass);
