aae308c2699eba6ac2155b0c2b04d5097bf25d6e
BAntDit <bantdit@gmail.com>
2020-03-04 23:07:53 +0300

    new default config

--- src/defaultConfigs.h
+++ src/defaultConfigs.h
@@ -5,10 +5,15 @@
 #ifndef CYCLONITE_DEFAULTCONFIGS_H
 #define CYCLONITE_DEFAULTCONFIGS_H
 
+#include "components/camera.h"
+#include "components/mesh.h"
+#include "components/transform.h"
+#include "components/transformStorage.h"
 #include "config.h"
-#include "core/camera.h"
-#include "core/transform.h"
 #include "platform.h"
+#include "systems/meshSystem.h"
+#include "systems/transferSystem.h"
+#include "systems/transformSystem.h"
 #include "systems/updateStages.h"
 #include "vulkan/androidSurface.h"
 #include "vulkan/win32Surface.h"
@@ -20,13 +25,13 @@
 namespace cyclonite {
 struct DefaultConfigs
 {
-    using ecs_config_t =
-      EcsConfig<easy_mp::type_list<core::Transform, core::PerspectiveCamera, core::OrthographicCamera>,
-                easy_mp::type_list<enttx::ComponentStorage<64, 8, core::Transform>,
-                                   enttx::ComponentStorage<8, 1, core::PerspectiveCamera>,
-                                   enttx::ComponentStorage<8, 1, core::OrthographicCamera>>,
-                easy_mp::type_list<>,
-                easy_mp::value_cast(UpdateStage::COUNT)>;
+    using ecs_config_t = EcsConfig<
+      easy_mp::type_list<components::Transform, components::Mesh, components::Camera>,
+      easy_mp::type_list<components::TransformStorage<32, 1>,
+                         enttx::ComponentStorage<16, 1, components::Mesh>,
+                         enttx::ComponentStorage<1, 1, components::Camera>>,
+      easy_mp::type_list<systems::TransformSystem, systems::MeshSystem, systems::CameraSystem, systems::TransferSystem>,
+      easy_mp::value_cast(UpdateStage::COUNT)>;
 };
 }
 
