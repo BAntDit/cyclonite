e9562126677a46ad0421223f9d41221bfe2dd352
BAntDit <bantdit@gmail.com>
2020-03-08 21:24:05 +0300

    config fixes

--- src/config.h
+++ src/config.h
@@ -6,7 +6,7 @@
 #define CYCLONITE_CONFIG_H
 
 #include <easy-mp/type_list.h>
-#include <enttx/config.h>
+#include <enttx/enttx.h>
 
 namespace cyclonite {
 template<typename ComponentList, typename ComponentStorageList, typename SystemList, size_t updateStageCount>
@@ -18,15 +18,16 @@ struct EcsConfig<easy_mp::type_list<Components...>,
                  easy_mp::type_list<Systems...>,
                  updateStageCount>
 {
-    using scene_component_list_t = easy_mp::type_list<Components...>;
-    using scene_storage_list_t = easy_mp::type_list<Storages...>;
-    using scene_system_list_t = easy_mp::type_list<Systems...>;
+    using component_list_t = easy_mp::type_list<Components...>;
+    using storage_list_t = easy_mp::type_list<Storages...>;
+    using system_list_t = easy_mp::type_list<Systems...>;
 
-    using entity_manager_t =
-      enttx::EntityManager<enttx::EntityManagerConfig<scene_component_list_t, scene_storage_list_t>>;
+    using entity_manager_config_t = enttx::EntityManagerConfig<component_list_t, storage_list_t>;
+
+    using entity_manager_t = enttx::EntityManager<entity_manager_config_t>;
 
     using system_manager_t =
-      enttx::EntityManager<enttx::SystemManagerConfig<updateStageCount, entity_manager_t, scene_system_list_t>>;
+      enttx::SystemManager<enttx::SystemManagerConfig<updateStageCount, entity_manager_config_t, system_list_t>>;
 };
 
 template<typename EcsConfig>
