d41bb568c7074f8f7ed4448a97bfaa5418862fbc
BAntDit <bantdit@gmail.com>
2020-03-18 01:10:55 +0300

    device lost error handling

--- src/vulkanRenderer.cpp
+++ src/vulkanRenderer.cpp
@@ -35,11 +35,35 @@ void VulkanRenderer::renderOneFrame(RenderPass& renderPass)
 
 void VulkanRenderer::finish() {
     auto gfxStop = taskManager_->submit([&, this]() -> void {
-        vkQueueWaitIdle(device_->graphicsQueue());
+        if (auto result = vkQueueWaitIdle(device_->graphicsQueue()); result != VK_SUCCESS) {
+            if (result == VK_ERROR_OUT_OF_HOST_MEMORY) {
+                throw std::runtime_error("could not wait until graphics queue gets idle, out of host memory");
+            }
+
+            if (result == VK_ERROR_OUT_OF_DEVICE_MEMORY) {
+                throw std::runtime_error("could not wait until graphics queue gets idle, out of device memory");
+            }
+
+            if (result == VK_ERROR_DEVICE_LOST) {
+                throw std::runtime_error("could not wait until graphics queue gets idle, device lost");
+            }
+        }
     });
 
     auto transferStop = taskManager_->submit([&, this]() -> void {
-        vkQueueWaitIdle(device_->hostTransferQueue());
+      if (auto result = vkQueueWaitIdle(device_->hostTransferQueue()); result != VK_SUCCESS) {
+          if (result == VK_ERROR_OUT_OF_HOST_MEMORY) {
+              throw std::runtime_error("could not wait until transfer queue gets idle, out of host memory");
+          }
+
+          if (result == VK_ERROR_OUT_OF_DEVICE_MEMORY) {
+              throw std::runtime_error("could not wait until transfer queue gets idle, out of device memory");
+          }
+
+          if (result == VK_ERROR_DEVICE_LOST) {
+              throw std::runtime_error("could not wait until transfer queue gets idle, device lost");
+          }
+      }
     });
 
     transferStop.get();
