742de2e2d62fc942bf7957606536868d310cf6be
BAntDit <bantdit@gmail.com>
2021-12-27 20:46:34 +0300

    workspace bug fixes

--- src/compositor/workspace.h
+++ src/compositor/workspace.h
@@ -6,6 +6,7 @@
 #define CYCLONITE_WORKSPACE_H
 
 #include "node.h"
+#include "nodeBuilder.h"
 #include "nodeInterface.h"
 
 namespace cyclonite::compositor {
@@ -20,15 +21,20 @@ public:
 
     void render(vulkan::Device& device);
 
+    template<typename NodeConfig, typename NodeTypeId>
+    auto get(type_pair<Node<NodeConfig>, NodeTypeId>) -> Node<NodeConfig>&;
+
+    template<typename NodeConfig, typename NodeTypeId>
+    [[nodiscard]] auto get(type_pair<Node<NodeConfig>, NodeTypeId>) const -> Node<NodeConfig> const&;
+
 public:
     class Builder
     {
     public:
         explicit Builder(vulkan::Device& device, uint8_t maxNodeCount = 10, size_t maxBytesPerNode = 64 * 1024);
 
-        template<typename NodeConfig, typename NodeFactory>
-        auto createNode(NodeFactory&& nodeFactory) -> std::enable_if_t<
-          // std::is_invocable_v<decltype(nodeFactory), cyclonite::compositor::BaseNode::Builder<NodeConfig>&&> &&
+        template<typename NodeConfig, typename NodeTypeId, typename NodeFactory>
+        auto createNode(type_pair<Node<NodeConfig>, NodeTypeId>, NodeFactory&& nodeFactory) -> std::enable_if_t<
           std::is_same_v<Node<NodeConfig>, std::decay_t<std::result_of_t<NodeFactory(vulkan::Device&)>>>,
           Builder&>;
 
@@ -46,6 +52,7 @@ public:
         std::vector<size_t> nodeSizes_;
         std::vector<size_t> nodeOffsets_;
         std::vector<NodeInterface> nodes_;
+        std::vector<uint64_t> nodeTypeIds_;
     };
 
 private:
@@ -59,6 +66,7 @@ private:
     uint8_t nodeCount_;
     std::vector<std::byte> nodeStorage_;
     std::vector<NodeInterface> nodes_;
+    std::vector<uint64_t> nodeTypeIds_;
 
     std::vector<VkPipelineStageFlags> nodeDstStageMasks_;
     std::vector<VkSemaphore> nodeWaitSemaphores_;
@@ -66,11 +74,10 @@ private:
     std::vector<VkSubmitInfo> submits_;
 };
 
-template<typename NodeConfig, typename NodeFactory>
-auto Workspace::Builder::createNode(NodeFactory&& nodeFactory) -> std::enable_if_t<
-  // std::is_invocable_v<decltype(nodeFactory), cyclonite::compositor::BaseNode::Builder<NodeConfig>&&> &&
-  std::is_same_v<Node<NodeConfig>, std::decay_t<std::result_of_t<NodeFactory(vulkan::Device&)>>>,
-  Builder&>
+template<typename NodeConfig, typename NodeTypeId, typename NodeFactory>
+auto Workspace::Builder::createNode(type_pair<Node<NodeConfig>, NodeTypeId>, NodeFactory&& nodeFactory)
+  -> std::enable_if_t<std::is_same_v<Node<NodeConfig>, std::decay_t<std::result_of_t<NodeFactory(vulkan::Device&)>>>,
+                      Builder&>
 {
     nodes_.emplace_back(
       new (allocateNode(sizeof(Node<NodeConfig>)))
@@ -92,7 +99,47 @@ auto Workspace::Builder::createNode(NodeFactory&& nodeFactory) -> std::enable_if
       },
       [](void* node) -> void { (reinterpret_cast<Node<NodeConfig>*>(node))->~Node<NodeConfig>(); });
 
+    nodeTypeIds_.emplace_back(NodeTypeId::value);
+
     return *this;
+}
+
+template<typename NodeConfig, typename NodeTypeId>
+auto Workspace::get(type_pair<Node<NodeConfig>, NodeTypeId>) -> Node<NodeConfig>&
+{
+    void* node = nullptr;
+
+    for (auto i = size_t{ 0 }, count = nodeTypeIds_.size(); i < count; i++) {
+        auto id = nodeTypeIds_[i];
+
+        if (id == NodeTypeId::value) {
+            node = nodes_[i].getRawPtr();
+            break;
+        }
+    }
+
+    assert(node != nullptr);
+
+    return *(reinterpret_cast<Node<NodeConfig>*>(node));
+}
+
+template<typename NodeConfig, typename NodeTypeId>
+auto Workspace::get(type_pair<Node<NodeConfig>, NodeTypeId>) const -> Node<NodeConfig> const&
+{
+    void const* node = nullptr;
+
+    for (auto i = size_t{ 0 }, count = nodeTypeIds_.size(); i < count; i++) {
+        auto id = nodeTypeIds_[i];
+
+        if (id == NodeTypeId::value) {
+            node = nodes_[i].getRawPtr();
+            break;
+        }
+    }
+
+    assert(node != nullptr);
+
+    return *(reinterpret_cast<Node<NodeConfig> const*>(node));
 }
 }
 
