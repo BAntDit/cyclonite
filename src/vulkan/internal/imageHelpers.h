78bdf71e79d83b3a010591870c54f5684da9e3ee
BAntDit <bantdit@example.com>
2022-01-09 15:39:40 +0300

    tmp fix for wrong final depth image layout

--- src/vulkan/internal/imageHelpers.h
+++ src/vulkan/internal/imageHelpers.h
@@ -38,7 +38,8 @@ inline auto _getDepthStencilImageLayoutByFormat(VkFormat format) -> VkImageLayou
         case VK_FORMAT_D16_UNORM:
         case VK_FORMAT_X8_D24_UNORM_PACK32:
         case VK_FORMAT_D32_SFLOAT:
-            layout = VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL;
+            // TODO:: add extension to separate depth and stencil
+            layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL; // VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL;
             break;
         case VK_FORMAT_S8_UINT:
             layout = VK_IMAGE_LAYOUT_STENCIL_ATTACHMENT_OPTIMAL;
