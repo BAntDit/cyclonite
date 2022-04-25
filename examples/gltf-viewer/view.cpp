f58eae408389b040063d8b64526a5b18868c4a6c
BAntDit <bantdit@example.com>
2022-04-25 14:17:15 +0300

    fixes missed render system initialization

--- examples/gltf-viewer/view.cpp
+++ examples/gltf-viewer/view.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "view.h"
+#include "appConfig.h"
 #include "model.h"
 
 namespace examples::viewer {
@@ -13,9 +14,21 @@ View::View() noexcept
   : workspace_{ nullptr }
 {}
 
-void View::init(std::shared_ptr<compositor::Workspace> const& workspace)
+void View::init(cyclonite::multithreading::TaskManager& taskManager,
+                cyclonite::vulkan::Device& device,
+                std::shared_ptr<compositor::Workspace> const& workspace)
 {
     workspace_ = workspace;
+
+    {
+        auto& node = workspace_->get(node_type_register_t::node_key_t<MainNodeConfig>{});
+        node.systems().get<systems::RenderSystem>().init(taskManager, device);
+    }
+
+    {
+        auto& node = workspace_->get(node_type_register_t::node_key_t<SurfaceNodeConfig>{});
+        node.systems().get<systems::RenderSystem>().init(taskManager, device);
+    }
 }
 
 void View::draw(cyclonite::vulkan::Device& device)
