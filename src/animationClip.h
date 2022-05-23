c6dccd02ad8da8180719177e6762b6df1eb37c9d
BAntDit <bantdit@gmail.com>
2022-05-23 12:12:39 +0300

    adds animation clip class (WIP)

---
+++ src/animationClip.h
@@ -0,0 +1,19 @@
+//
+// Created by anton on 5/22/22.
+//
+
+#ifndef CYCLONITE_ANIMATION_CLIP_H
+#define CYCLONITE_ANIMATION_CLIP_H
+
+#include <vector>
+
+namespace cyclonite
+{
+class AnimationClip
+{
+public:
+private:
+};
+}
+
+#endif // CYCLONITE_ANIMATION_H
