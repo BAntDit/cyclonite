c60d736e9cd7c71542bc66d2adc545c9e7a89729
BAntDit <bantdit@gmail.com>
2022-12-11 23:02:55 +0300

    fix: fixes an issue when Clang compiler can not deduce config alias types.

--- src/config.h
+++ src/config.h
@@ -41,8 +41,13 @@ template<typename... C, typename... S, template<typename, typename> typename Spe
 struct is_component_config_specialization<Spec<type_list<C...>, type_list<S...>>, Spec> : std::true_type
 {};
 
-template<typename T, template<typename, typename> typename Spec>
-inline constexpr auto is_component_config_specialization_v = is_component_config_specialization<T, Spec>::value;
+// enttx::EntityManagerConfig instead of component_config_t according to paragraph 14.5.7/2 of the C++ Standard
+// When a template-id refers to the specialization of an alias template, it is equivalent to the associated type
+// obtained by substitution of its template-arguments for the template-parameters in the type-id of the alias template.
+// [ Note: An alias template name is never deduced.—end note ]
+template<typename T>
+inline constexpr auto is_component_config_specialization_v =
+  is_component_config_specialization<T, enttx::EntityManagerConfig>::value;
 
 template<typename T, template<size_t, typename> typename Spec>
 struct is_systems_config_specialization : std::false_type
@@ -52,15 +57,20 @@ template<size_t stageCount, typename... Systems, template<size_t, typename> type
 struct is_systems_config_specialization<Spec<stageCount, type_list<Systems...>>, Spec> : std::true_type
 {};
 
-template<typename T, template<size_t, typename> typename Spec>
-inline constexpr auto is_systems_config_specialization_v = is_systems_config_specialization<T, Spec>::value;
+// enttx::SystemManagerConfig instead of systems_config_t according to paragraph 14.5.7/2 of the C++ Standard
+// When a template-id refers to the specialization of an alias template, it is equivalent to the associated type
+// obtained by substitution of its template-arguments for the template-parameters in the type-id of the alias template.
+// [ Note: An alias template name is never deduced.—end note ]
+template<typename T>
+inline constexpr auto is_systems_config_specialization_v =
+  is_systems_config_specialization<T, enttx::SystemManagerConfig>::value;
 }
 
 template<typename T>
-concept ComponentConfig = internal::is_component_config_specialization_v<T, component_config_t>;
+concept ComponentConfig = internal::is_component_config_specialization_v<T>;
 
 template<typename T>
-concept SystemsConfig = internal::is_systems_config_specialization_v<T, systems_config_t>;
+concept SystemsConfig = internal::is_systems_config_specialization_v<T>;
 
 using default_component_config_t =
   component_config_t<type_list<components::Transform, components::Mesh, components::Camera, components::Animator>,
@@ -68,41 +78,17 @@ using default_component_config_t =
                                components::MeshStorage<1024>,
                                enttx::ComponentStorage<1, 1, components::Camera>,
                                components::AnimatorStorage>>;
+// test:
+static_assert(internal::is_component_config_specialization_v<default_component_config_t>);
 
 using default_systems_config_t = systems_config_t<
   value_cast(UpdateStage::COUNT),
   type_list<systems::AnimationSystem, systems::TransformSystem, systems::CameraSystem, systems::MeshSystem>>;
 
+// test:
+static_assert(internal::is_systems_config_specialization_v<default_systems_config_t>);
+
 namespace config_traits {
-template<typename T>
-struct get_max_wait_semaphore_count
-{
-private:
-    using yes_t = uint8_t;
-    using no_t = uint16_t;
-
-    template<typename C>
-    static constexpr auto test_field(std::integral_constant<size_t, C::max_wait_semaphore_count_v>*) -> yes_t;
-
-    template<typename C>
-    static constexpr auto test_filed(...) -> no_t;
-
-    static constexpr auto has_max_wait_semaphore_count_field = sizeof(test_field<T>(0)) == sizeof(yes_t);
-
-public:
-    static constexpr auto value() -> size_t
-    {
-        if constexpr (has_max_wait_semaphore_count_field) {
-            return T::max_wait_semaphore_count_v;
-        } else {
-            return size_t{ 16 };
-        }
-    }
-};
-
-template<typename T>
-inline constexpr auto max_wait_semaphore_count_v = get_max_wait_semaphore_count<T>::value();
-
 template<typename T>
 struct is_logic_node
 {
@@ -134,6 +120,39 @@ inline constexpr auto is_logic_node_v = is_logic_node<T>::value();
 
 template<typename T>
 inline constexpr auto is_graphics_node_v = !is_logic_node<T>::value();
+
+template<typename T>
+struct get_max_wait_semaphore_count
+{
+private:
+    using yes_t = uint8_t;
+    using no_t = uint16_t;
+
+    template<typename C>
+    static constexpr auto test_field(std::integral_constant<size_t, C::max_wait_semaphore_count_v>*) -> yes_t;
+
+    template<typename C>
+    static constexpr auto test_filed(...) -> no_t;
+
+    static constexpr auto has_max_wait_semaphore_count_field = sizeof(test_field<T>(0)) == sizeof(yes_t);
+
+public:
+    static constexpr auto value() -> size_t
+    {
+        if constexpr (is_graphics_node_v<T>) {
+            if constexpr (has_max_wait_semaphore_count_field) {
+                return T::max_wait_semaphore_count_v;
+            } else {
+                return size_t{ 16 };
+            }
+        } else {
+            return 0;
+        }
+    }
+};
+
+template<typename T>
+inline constexpr auto max_wait_semaphore_count_v = get_max_wait_semaphore_count<T>::value();
 
 template<typename T>
 struct is_surface_node
@@ -195,14 +214,10 @@ inline constexpr auto pass_count_v = get_pass_count<T>::value();
 }
 
 template<ComponentConfig CompConfig, SystemsConfig SysConfig>
-struct Config;
-
-template<typename... Components, typename... Storages, typename... Systems, size_t updateStageCount>
-struct Config<component_config_t<type_list<Components...>, type_list<Storages...>>,
-              systems_config_t<updateStageCount, type_list<Systems...>>>
+struct Config
 {
-    using component_config_t = component_config_t<type_list<Components...>, type_list<Storages...>>;
-    using systems_config_t = systems_config_t<updateStageCount, type_list<Systems...>>;
+    using component_config_t = CompConfig;
+    using systems_config_t = SysConfig;
 };
 
 namespace node_internal {
@@ -214,12 +229,12 @@ template<ComponentConfig CompConfig, SystemsConfig SysConfig, template<typename,
 struct is_node_config_specialization<Spec<CompConfig, SysConfig>, Spec> : std::true_type
 {};
 
-template<typename T, template<typename, typename> typename Spec>
-inline constexpr auto is_node_config_specialization_v = is_node_config_specialization<T, Spec>::value;
+template<typename T>
+inline constexpr auto is_node_config_specialization_v = is_node_config_specialization<T, Config>::value;
 }
 
 template<typename T>
-concept NodeConfig = node_internal::is_node_config_specialization_v<T, Config> &&
+concept NodeConfig = node_internal::is_node_config_specialization_v<T> &&
                      ((config_traits::is_logic_node<T>::value() && !config_traits::is_surface_node<T>::value() &&
                        config_traits::get_pass_count<T>::value() == 0) ||
                       !config_traits::is_logic_node<T>::value());
