79b59992a1d864494adc2bab0e6cac09c6db8eaf
BAntDit <bantdit@gmail.com>
2023-02-20 21:28:55 +0300

    batching (wip) :)

---
+++ src/render/batch.h
@@ -0,0 +1,17 @@
+//
+// Created by anton on 2/20/23.
+//
+
+#ifndef CYCLONITE_BATCH_H
+#define CYCLONITE_BATCH_H
+
+namespace cyclonite::render
+{
+class Batch
+{
+public:
+    // TODO::
+};
+}
+
+#endif // CYCLONITE_BATCH_H
