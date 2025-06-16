983459d88cc145bb7637c723ad2e34d619376bd1
BAntDit <bantdit@gmail.com>
2025-06-16 22:44:42 +0300

    feat(gfx): gfx::Instance concept now requires instance to implement the method ::createDevice

--- src/gfx/interfaces/instanceInterface.h
+++ src/gfx/interfaces/instanceInterface.h
@@ -5,15 +5,26 @@
 #ifndef GFX_INTERFACES_INSTANCE_H
 #define GFX_INTERFACES_INSTANCE_H
 
+#include "resourceRef.h"
 #include <concepts>
+#include <utility>
 
 namespace cyclonite::gfx::interfaces {
 template<typename T>
-concept InstanceConcept = true;
+concept InstanceConcept = requires {
+                              requires std::is_member_function_pointer_v<decltype(&T::createDevice)>;
+
+                              []<typename Ret, typename... Args>(Ret (T::*)(Args && ...)) constexpr -> bool {
+                                  return std::is_same_v<Ret, gfx::ResourceRef>;
+                              }(&T::createDevice);
+                          };
 
 template<InstanceConcept InstanceImplementation>
 class InstanceInterface : private InstanceImplementation
-{};
+{
+public:
+    using InstanceImplementation::createDevice;
+};
 }
 
 #endif // GFX_INTERFACES_INSTANCE_H
