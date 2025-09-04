13fd1fd4c1f19de6220338b504e26906d3903eec
asedov <asedov@saber.games>
2025-09-04 14:52:21 +0400

    fixes device concept

--- src/gfx/interfaces/deviceInterface.h
+++ src/gfx/interfaces/deviceInterface.h
@@ -10,32 +10,38 @@
 #include <concepts>
 #include <string_view>
 #include <utility>
+#include <metrix/type_traits.h>
 
 namespace cyclonite::gfx::interfaces {
 template<typename T>
-concept DeviceConcept = requires(T t, uint32_t a, uint32_t b, std::string_view s, SurfaceFlagBits f) {
-                            {
-                                t.resourceBase()
-                                } -> std::same_as<core::ResourceBase*>;
-                            {
-                                t.name()
-                                } -> std::same_as<std::string_view>;
-                            {
-                                t.vendor()
-                                } -> std::same_as<DeviceVendor>;
-                            {
-                                t.limits()
-                                } -> std::same_as<DeviceLimits const&>;
-                            {
-                                t.createRenderWindow(a, b, s, f)
-                                } -> std::same_as<core::ResourceRef>;
+concept DeviceConcept = requires(T t, uint32_t a, uint32_t b, std::string_view s, SurfaceFlagBits f)
+{
+    {
+        t.resourceBase()
+    }
+    ->std::same_as<core::ResourceBase*>;
+    {
+        t.name()
+    }
+    ->std::same_as<std::string_view>;
+    {
+        t.vendor()
+    }
+    ->std::same_as<DeviceVendor>;
+    {
+        t.limits()
+    }
+    ->std::same_as<DeviceLimits const&>;
+    {
+        t.createRenderWindow(a, b, s, f)
+    }
+    ->std::same_as<core::ResourceRef>;
 
-                            requires std::is_member_function_pointer_v<decltype(&T::createRenderPassWithRTVs)>;
+    requires std::is_member_function_pointer_v<decltype(&T::createRenderPassWithRTVs)>;
 
-                            requires []<typename Ret, typename... Args>(Ret (T::*)(Args...)) constexpr -> bool {
-                                return std::is_same_v<Ret, core::ResourceRef>;
-                            }(&T::createRenderPassWithRTVs);
-                        };
+    requires std::is_same_v<core::ResourceRef,
+                            metrix::member_function_return_type_t<decltype(&T::createRenderPassWithRTVs)>>;
+};
 
 template<DeviceConcept PlatformImplementation>
 class DeviceInterface : private PlatformImplementation
