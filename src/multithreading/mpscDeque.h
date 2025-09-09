9d2ea467014fc8c86deaffd519936d9ab46ddebc
asedov <asedov@saber.games>
2025-09-09 16:01:15 +0400

    mpsqDeque update

--- src/multithreading/mpscDeque.h
+++ src/multithreading/mpscDeque.h
@@ -8,6 +8,7 @@
 #include "common.h"
 #include <atomic>
 #include <cstdint>
+#include <optional>
 
 namespace cyclonite::multithreading {
 template<DequeItemConcept DataItemType>
@@ -28,12 +29,47 @@ public:
     template<typename... Args>
     auto tryEmplace(Args&&... args) -> bool;
 
+    // can be called in consumer thread only
+    auto tryPop() -> std::optional<DataItemType>;
+
 private:
     alignas(hardware_destructive_interference_size) std::atomic<uint64_t> consumerTop_;
     alignas(hardware_destructive_interference_size) std::atomic<uint64_t> consumerBottom_;
     alignas(hardware_destructive_interference_size) std::atomic<uint64_t> producerBottom_;
     alignas(hardware_destructive_interference_size) DequeData<DataItemType>* data_;
 };
+
+template<DequeItemConcept DataItemType>
+MpscDeque<DataItemType>::MpscDeque(size_t capacity)
+  : consumerTop_{ 0 }
+  , consumerBottom_{ 0 }
+  , producerBottom_{ 0 }
+  , data_{ new data_{ capacity } }
+{}
+
+template<DequeItemConcept DataItemType>
+MpscDeque<DataItemType>::~MpscDeque()
+{
+    delete data_;
+}
+
+template<DequeItemConcept DataItemType>
+auto MpscDeque<DataItemType>::countItems() const -> size_t
+{
+    auto bottom = consumerBottom_.load(std::memory_order_acquire);
+    auto top = consumerTop_.load(std::memory_order_acquire);
+
+    return bottom - top;
+}
+
+template<DequeItemConcept DataItemType>
+auto MpscDeque<DataItemType>::isEmpty() const -> bool
+{
+    auto bottom = producerBottom_.load(std::memory_order_acquire);
+    auto top = consumerTop_.load(std::memory_order_acquire);
+
+    return (bottom - top) == 0;
+}
 
 template<DequeItemConcept DataItemType>
 template<typename... Args>
@@ -56,6 +92,22 @@ auto MpscDeque<DataItemType>::tryEmplace(Args&&... args) -> bool
     }
 
     return false;
+}
+
+template<DequeItemConcept DataItemType>
+auto MpscDeque<DataItemType>::tryPop() -> std::optional<DataItemType>
+{
+    auto result = std::optional<DataItemType>{ std::nullopt };
+
+    auto consumerBottom = consumerBottom_.load(std::memory_order_acquire);
+    auto consumerTop = consumerTop_.load(std::memory_order_acquire);
+
+    if (consumerTop != consumerBottom) {
+        result = data_->load(consumerTop);
+        consumerTop_.fetch_add(1, std::memory_order_release);
+    }
+
+    return result;
 }
 }
 
