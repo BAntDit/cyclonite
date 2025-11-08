ea155b5b05c545465fd189c8569d81cc778da038
BAntDit <bantdit@gmail.com>
2025-11-08 21:27:36 +0300

    adds descriptor type enum

--- src/gfx/common.h
+++ src/gfx/common.h
@@ -254,6 +254,22 @@ enum class StencilOp : uint8_t
     DECREMENT_AND_WRAP = 7
 };
 
+enum class DescriptorType : uint_fast8_t
+{
+    SAMPLER = 0,
+    COMBINED_IMAGE_SAMPLER = 1,
+    SAMPLED_IMAGE = 2,
+    STORAGE_IMAGE = 3,
+    UNIFORM_TEXEL_BUFFER = 4,
+    STORAGE_TEXEL_BUFFER = 5,
+    UNIFORM_BUFFER = 6,
+    STORAGE_BUFFER = 7,
+    UNIFORM_BUFFER_DYNAMIC = 8,
+    STORAGE_BUFFER_DYNAMIC = 9,
+    INPUT_ATTACHMENT = 10,
+    INLINE_UNIFORM_ATTACHMENT = 11
+};
+
 enum class GpuMemoryAllocationFlags : uint8_t
 {
     DEDICATED_MEMORY = 1 << 0,
