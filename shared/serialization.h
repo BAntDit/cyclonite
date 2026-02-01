48cfffe419c01a16313656e065c7b012dbb528bc
BAntDit <bantdit@gmail.com>
2026-02-01 22:05:21 +0400

    serialization interface update

--- shared/serialization.h
+++ shared/serialization.h
@@ -7,99 +7,19 @@
 
 #include <array>
 #include <concepts>
+#include <cstddef>
+#include <cstdint>
+#include <metrix/containers.h>
+#include <metrix/type_traits.h>
 #include <tuple>
 #include <type_traits>
-#include <cstddef>
+#include <unistd.h>
+#include <vector>
 
-// serialization based on https://github.com/mera-company/cpp-serialization-library/tree/master
 namespace cyclonite::shared {
-template<typename T>
-concept SerializationDataWriterConcept = std::invocable<T>;
-
 namespace internal {
-template<auto... Values>
-struct value_list
-{};
-}
-
-template<typename T, SerializationDataWriterConcept SerializationWriter>
-class SerializationInvoker
-{
-public:
-    template<auto... Accessor>
-    explicit constexpr SerializationInvoker(internal::value_list<Accessor...>)
-      : invokePtr_{ &serializationInvoke<Accessor...> }
-    {
-    }
-
-    constexpr void operator()(T& t, SerializationWriter& sw) { (*invokePtr_)(t, sw); }
-
-private:
-    template<auto... Accessor>
-    static constexpr void serializationInvoke(T& t, SerializationWriter& si);
-
-    using serialization_invoke_ptr_t = void (*)(T&, SerializationWriter&);
-
-    serialization_invoke_ptr_t invokePtr_;
-};
-
-namespace internal {
-template<typename Ret, typename T, typename... Args>
-struct accessor_method_info_t
-{
-    using return_type_t = Ret;
-    using class_type_t = T;
-    using arguments_t = std::tuple<Args...>;
-    using decayed_arguments_t = std::tuple<std::remove_pointer_t<std::decay_t<Args>>...>;
-};
-
-template<typename>
-struct accessor_info_t;
-
-// all overloads, but no volatile
-// (volatile can be added later, if necessary)
-template<typename Ret, typename T, typename... Args>
-struct accessor_info_t<Ret (T::*)(Args...)> : accessor_method_info_t<Ret, T, Args...>
-{};
-
-template<typename Ret, typename Class, typename... Args>
-struct accessor_info_t<Ret (Class::*)(Args...) noexcept> : accessor_method_info_t<Ret, Class, Args...>
-{};
-
-template<typename Ret, typename Class, typename... Args>
-struct accessor_info_t<Ret (Class::*)(Args...)&> : accessor_method_info_t<Ret, Class, Args...>
-{};
-
-template<typename Ret, typename Class, typename... Args>
-struct accessor_info_t<Ret (Class::*)(Args...) & noexcept> : accessor_method_info_t<Ret, Class, Args...>
-{};
-
-template<typename Ret, typename Class, typename... Args>
-struct accessor_info_t<Ret (Class::*)(Args...) &&> : accessor_method_info_t<Ret, Class, Args...>
-{};
-
-template<typename Ret, typename Class, typename... Args>
-struct accessor_info_t<Ret (Class::*)(Args...) && noexcept> : accessor_method_info_t<Ret, Class, Args...>
-{};
-
-template<typename Ret, typename Class, typename... Args>
-struct accessor_info_t<Ret (Class::*)(Args...) const> : accessor_method_info_t<Ret, Class, Args...>
-{};
-
-template<typename Ret, typename Class, typename... Args>
-struct accessor_info_t<Ret (Class::*)(Args...) const noexcept> : accessor_method_info_t<Ret, Class, Args...>
-{};
-
-template<typename Ret, typename Class, typename... Args>
-struct accessor_info_t<Ret (Class::*)(Args...) const&> : accessor_method_info_t<Ret, Class, Args...>
-{};
-
-template<typename Ret, typename Class, typename... Args>
-struct accessor_info_t<Ret (Class::*)(Args...) const & noexcept> : accessor_method_info_t<Ret, Class, Args...>
-{};
-
-template<typename Ret, typename Class, typename... Args>
-struct accessor_info_t<Ret (Class::*)(Args...) const && noexcept> : accessor_method_info_t<Ret, Class, Args...>
+template<typename StreamWriter>
+struct stream_writer_type_wrap_t
 {};
 
 template<typename... Accessors>
@@ -108,11 +28,51 @@ struct first_accessor_class_type;
 template<typename First, typename... Rest>
 struct first_accessor_class_type<First, Rest...>
 {
-    using type = typename accessor_info_t<First>::class_type_t;
+    using type = typename metrix::member_function_class_type_t<First>;
 };
 
 template<typename... Accessors>
 using first_accessor_class_type_t = typename first_accessor_class_type<Accessors...>::type;
+
+template<auto... Values>
+struct value_list
+{};
+
+template<typename ArgumentsList>
+struct args_tuple;
+
+template<typename... Args>
+struct args_tuple<metrix::type_list<Args...>>
+{
+    using type = std::tuple<std::remove_pointer_t<std::decay_t<Args>>...>;
+};
+
+template<typename ArgumentsList>
+using args_tuple_t = typename args_tuple<ArgumentsList>::type;
+
+template<typename ArgumentsList>
+struct decayed_args_tuple;
+
+template<typename... Args>
+struct decayed_args_tuple<metrix::type_list<Args...>>
+{
+    using type = std::tuple<Args...>;
+};
+
+template<typename ArgumentsList>
+using decayed_args_tuple_t = typename decayed_args_tuple<ArgumentsList>::type;
+
+template<typename TupleType, typename T>
+struct has_element;
+
+template<typename... Element, typename T>
+struct has_element<std::tuple<Element...>, T>
+{
+    static constexpr bool value = metrix::type_list<Element...>::template has_type<T>::value;
+};
+
+template<typename TupleType, typename T>
+inline constexpr bool has_element_v = has_element<TupleType, T>::value;
 
 template<typename Type, typename PassedType>
 inline constexpr auto passArgument(PassedType&& val) -> decltype(auto)
@@ -125,144 +85,235 @@ inline constexpr auto passArgument(PassedType&& val) -> decltype(auto)
 }
 
 template<typename Accessor>
-class SerializationChainItem
+class AccessChainItem
 {
 public:
-    using decayed_args_tuple_t = typename accessor_info_t<Accessor>::decayed_arguments_t;
-    using args_tuple_t = typename accessor_info_t<Accessor>::arguments_t;
-    using class_t = typename accessor_info_t<Accessor>::class_type_t;
+    using class_t = metrix::member_function_class_type_t<Accessor>;
+    using args_tuple_t = args_tuple_t<metrix::member_function_argument_type_list_t<Accessor>>;
+    using decayed_args_tuple_t = decayed_args_tuple_t<metrix::member_function_argument_type_list_t<Accessor>>;
 
     static constexpr size_t args_count_v = std::tuple_size_v<args_tuple_t>;
 
-    template<typename T>
-    constexpr SerializationChainItem(Accessor const& accessor, T& t);
+    decayed_args_tuple_t arguments;
 
-    args_tuple_t arguments;
+    template<typename AnyObject>
+    AccessChainItem(Accessor const& accessor, AnyObject& anyObject);
 
     template<typename NextAccessor>
-    constexpr auto operator<<(NextAccessor const& nextAccessor) && -> SerializationChainItem<NextAccessor>
+    auto operator<<(NextAccessor& nextAccessor) &&;
+
+    auto getArguments()
     {
-        using invoking_class_t = typename accessor_info_t<NextAccessor>::class_type_t;
-        return SerializationChainItem<NextAccessor>{ nextAccessor, std::get<invoking_class_t>(arguments) };
+        using args_vector_t = std::vector<typename AccessChainItem<Accessor>::decayed_args_tuple_t>;
+
+        auto res = args_vector_t{};
+        res.emplace_back(arguments);
+
+        return res;
     }
 
 private:
-    template<typename T, size_t... Idx>
-    constexpr void invokeImpl(Accessor const& accessor, T& t, std::index_sequence<Idx...>);
+    template<typename AnyObject, size_t... Idx>
+    void invokeImpl(Accessor const& accessor, AnyObject& anyObject, std::index_sequence<Idx...>);
 };
 
 template<typename Accessor>
-template<typename T, size_t... Idx>
-constexpr void SerializationChainItem<Accessor>::invokeImpl(Accessor const& accessor, T& t, std::index_sequence<Idx...>)
+class AccessChainItemArray
 {
-    (t.*accessor)(passArgument<std::tuple_element_t<Idx, decayed_args_tuple_t>>(std::get<Idx>(arguments))...);
+public:
+    AccessChainItemArray() = default;
+
+    template<typename AnyObject>
+    void addAccessChainItem(Accessor const& accessor, AnyObject& anyObject);
+
+    template<typename NextAccessor>
+    auto operator<<(NextAccessor& nextAccessor) && -> AccessChainItemArray<NextAccessor>;
+
+    auto getArguments()
+    {
+        using args_vector_t = std::vector<typename AccessChainItem<Accessor>::decayed_args_tuple_t>;
+
+        auto res = args_vector_t{};
+        res.reserve(accessChainItems_.size());
+
+        for (auto& accessChainItem : accessChainItems_) {
+            res.emplace_back(accessChainItem.arguments);
+        }
+
+        return res;
+    }
+
+private:
+    std::vector<AccessChainItem<Accessor>> accessChainItems_;
+};
+
+template<typename Accessor>
+template<typename AnyObject>
+void AccessChainItemArray<Accessor>::addAccessChainItem(Accessor const& accessor, AnyObject& anyObject)
+{
+    accessChainItems_.emplace_back(accessor, anyObject);
 }
 
 template<typename Accessor>
-template<typename T>
-constexpr SerializationChainItem<Accessor>::SerializationChainItem(Accessor const& accessor, T& t)
+template<typename NextAccessor>
+auto AccessChainItemArray<Accessor>::operator<<(NextAccessor& nextAccessor) && -> AccessChainItemArray<NextAccessor>
 {
-    invokeImpl(accessor, t, std::make_index_sequence<args_count_v>{});
+    using invoking_class_t = typename metrix::member_function_class_type_t<NextAccessor>;
+
+    auto accessChainItemArray = AccessChainItemArray<NextAccessor>{};
+    for (auto& accessChainItem : accessChainItems_) {
+        accessChainItemArray.addAccessChainItem(nextAccessor, std::get<invoking_class_t>(accessChainItem.arguments));
+    }
+
+    return accessChainItemArray;
 }
 
-template<typename T>
-struct SerializationChainBeginner
+// AccessChainItem methods:
+template<typename Accessor>
+template<typename AnyObject>
+AccessChainItem<Accessor>::AccessChainItem(Accessor const& accessor, AnyObject& anyObject)
+  : arguments{}
 {
-    explicit constexpr SerializationChainBeginner(T& t)
-      : t_(t)
+    invokeImpl(accessor, anyObject, arguments, std::make_index_sequence<args_count_v>{});
+}
+
+template<typename Accessor>
+template<typename AnyObject, size_t... Idx>
+void AccessChainItem<Accessor>::invokeImpl(Accessor const& accessor, AnyObject& anyObject, std::index_sequence<Idx...>)
+{
+    (anyObject.*accessor)(passArgument<std::tuple_element_t<Idx, args_tuple_t>>(std::get<Idx>(arguments))...);
+}
+
+template<typename Accessor>
+template<typename NextAccessor>
+auto AccessChainItem<Accessor>::operator<<(NextAccessor& nextAccessor) &&
+{
+    using invoking_class_t = typename metrix::member_function_class_type_t<NextAccessor>;
+
+    if constexpr (has_element_v<decayed_args_tuple_t, invoking_class_t>) {
+        return AccessChainItem<NextAccessor>(nextAccessor, std::get<invoking_class_t>(arguments));
+    } else if constexpr (has_element_v<decayed_args_tuple_t, std::vector<invoking_class_t>>) {
+        auto accessChainItemArray = AccessChainItemArray<NextAccessor>{};
+        auto& objectArray = std::get<std::vector<invoking_class_t>>(arguments);
+        for (auto& anyObject : objectArray) {
+            accessChainItemArray.addAccessChainItem(nextAccessor, anyObject);
+        }
+        return accessChainItemArray;
+    } /*else if extends for other types here*/
+}
+//
+
+template<typename AnyObject>
+class AccessChainBeginner
+{
+public:
+    explicit AccessChainBeginner(AnyObject& anyObject)
+      : anyObject_{ anyObject }
     {
     }
 
     template<typename NextAccessor>
-    constexpr auto operator<<(NextAccessor const& nextAccessor) -> SerializationChainItem<NextAccessor>
+    auto operator<<(NextAccessor const& nextAccessor) -> AccessChainItem<NextAccessor>
     {
-        return SerializationChainItem<NextAccessor>{ nextAccessor, t_ };
+        return AccessChainItem<NextAccessor>{ anyObject_, nextAccessor };
     }
 
 private:
-    T& t_;
+    AnyObject& anyObject_;
 };
 
-template<typename T, typename... Accessor>
-constexpr auto invokeSerializationChain(T&& t, Accessor&&... accessor)
+template<typename AnyObject, typename... Accessor>
+auto invokeAccessChain(AnyObject&& anyObject, Accessor&&... accessor)
 {
-    return (SerializationChainBeginner<std::decay_t<T>>{ t } << ... << std::forward<Accessor>(accessor)).arguments;
+    return (AccessChainBeginner<std::decay_t<AnyObject>>{ anyObject } << ... << std::forward<Accessor>(accessor))
+      .getArguments();
 }
 
-template<SerializationDataWriterConcept SerializationWriter>
-struct serialization_interface_wrap_t
-{};
+template<typename AnyObject, typename StreamWriter>
+class AccessChainInvoker
+{
+public:
+    template<auto... Accessor>
+    explicit AccessChainInvoker(value_list<Accessor...>);
+
+    void operator()(AnyObject& anyObject, StreamWriter& streamWriter) const;
+
+private:
+    template<auto... Accessor>
+    static void accessChainInvoke(AnyObject& anyObject, StreamWriter& streamWriter);
+
+private:
+    using access_chain_invoker_f = void (*)(AnyObject&, StreamWriter&);
+
+    access_chain_invoker_f invoke_;
+};
+
+template<typename AnyObject, typename StreamWriter>
+template<auto... Accessor>
+/*explicit*/ AccessChainInvoker<AnyObject, StreamWriter>::AccessChainInvoker(value_list<Accessor...>)
+  : invoke_{ &accessChainInvoke<Accessor...> }
+{
+}
+
+template<typename AnyObject, typename StreamWriter>
+template<auto... Accessor>
+/*static*/ void AccessChainInvoker<AnyObject, StreamWriter>::accessChainInvoke(AnyObject& anyObject,
+                                                                               StreamWriter& streamWriter)
+{
+    auto&& argsVec = invokeAccessChain(anyObject, Accessor...);
+    for (auto&& args : argsVec) {
+        streamWriter << args;
+    }
+}
+
+template<typename AnyObject, typename StreamWriter>
+void AccessChainInvoker<AnyObject, StreamWriter>::operator()(AnyObject& anyObject, StreamWriter& streamWriter) const
+{
+    invoke_(anyObject, streamWriter);
+}
 
 template<auto... Accessor>
-struct SerilalizationInvokeForwarder
+struct AccessChainInvokeForwarder
 {
-    template<SerializationDataWriterConcept SerializationWriter>
-    constexpr auto serializationInvoker() const noexcept
-    {
-        return SerializationInvoker<first_accessor_class_type<decltype(Accessor)...>, SerializationWriter>{
-            value_list<Accessor...>{}
-        };
+    template<typename StreamWriter>
+    auto invoke() const {
+        using object_type_t = typename metrix::type_list<decltype(Accessor)...>::template get_type<0>::type;
+        return AccessChainInvoker<object_type_t, StreamWriter>{ value_list<Accessor...>{} };
     }
 };
 }
 
-template<SerializationDataWriterConcept SerializationWriter, typename... Args>
-constexpr inline auto useWriter(Args&&... args) -> internal::serialization_interface_wrap_t<SerializationWriter>
-{
-    return internal::serialization_interface_wrap_t<SerializationWriter>(std::forward<Args>(args)...);
-}
-
-template<typename T, SerializationDataWriterConcept SerializationWriter>
 template<auto... Accessor>
-/*static*/ constexpr void SerializationInvoker<T, SerializationWriter>::serializationInvoke(T& t,
-                                                                                            SerializationWriter& sw)
+constexpr auto makeAccessChain()
 {
-    sw(internal::invokeSerializationChain(t, Accessor...));
+    return internal::AccessChainInvokeForwarder<Accessor...>{};
 }
 
-template<auto... Accessor>
-constexpr auto makeSerialzationDataAccessChain()
-{
-    return internal::SerilalizationInvokeForwarder<Accessor...>{};
-}
-
-template<typename T, size_t N, SerializationDataWriterConcept SerializationWriter>
+template<typename AnyObject, size_t N, typename StreamWriter>
 class Serializer
 {
 public:
-    using data_access_chain_t = SerializationInvoker<T, SerializationWriter>;
+    using data_access_chain_t = internal::AccessChainInvoker<AnyObject, StreamWriter>;
 
     template<typename... AccessChain>
-    explicit constexpr Serializer(internal::serialization_interface_wrap_t<SerializationWriter>,
-                                  AccessChain&&... accessChain);
+    constexpr Serializer(internal::stream_writer_type_wrap_t<StreamWriter>, AccessChain&&... accessChain); // TODO::
 
-    constexpr void operator()(T& t, SerializationWriter& si) const;
+    void operator()(AnyObject& anyObject, StreamWriter& sw) const; // TODO:: ...
 
 private:
     std::array<data_access_chain_t, N> accessChains_;
 };
 
-template<typename T, size_t N, SerializationDataWriterConcept SerializationWriter>
-template<typename... AccessChain>
-constexpr Serializer<T, N, SerializationWriter>::Serializer(
-  internal::serialization_interface_wrap_t<SerializationWriter>,
-  AccessChain&&... accessChain)
-  : accessChains_{ accessChain.template serializationInvoker<SerializationWriter>()... }
+template<typename StreamWriter>
+constexpr inline auto useWriter() -> internal::stream_writer_type_wrap_t<StreamWriter>
 {
+    return internal::stream_writer_type_wrap_t<StreamWriter>{};
 }
 
-template<typename T, size_t N, SerializationDataWriterConcept SerializationWriter>
-constexpr void Serializer<T, N, SerializationWriter>::operator()(T& t, SerializationWriter& sw) const
-{
-    for (auto const& accessChainInvoke : accessChains_) {
-        accessChainInvoke(t, sw);
-    }
-}
-
-// deduction guide
-template<SerializationDataWriterConcept SerializationWriter, typename... AccessChain>
-explicit Serializer(internal::serialization_interface_wrap_t<SerializationWriter>, AccessChain...)
-  -> Serializer<internal::first_accessor_class_type_t<AccessChain...>, sizeof...(AccessChain), SerializationWriter>;
+// deduction guide:
+template<typename StreamWriter, typename... AccessChain>
+Serializer(internal::stream_writer_type_wrap_t<StreamWriter>, AccessChain&&... accessChain)
+  -> Serializer<internal::first_accessor_class_type_t<AccessChain...>, sizeof...(AccessChain), StreamWriter>;
 }
 
 #endif // CYCLONITE_SHARED_SERIALIZATION_H
