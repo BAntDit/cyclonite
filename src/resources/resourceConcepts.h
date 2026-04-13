c5951aa72bc9ae937caad0b9dacd4ddc71b1bbfe
BAntDit <bantdit@gmail.com>
2026-04-13 18:13:04 +0400

    resource management update

---
+++ src/resources/resourceConcepts.h
@@ -0,0 +1,32 @@
+//
+// Created by anton on 4/13/26.
+//
+
+#ifndef CYCLONITE_RESOURCES_RESOURCE_CONCEPTS_H
+#define CYCLONITE_RESOURCES_RESOURCE_CONCEPTS_H
+
+#include "managedResource.h"
+#include "resourceGroupBase.h"
+#include <concepts>
+#include <future>
+#include <metrix/type_traits.h>
+#include <type_traits>
+
+namespace cyclonite::resources {
+template<typename T>
+concept ManagedResourceConcept = requires(T t) { requires std::is_base_of_v<ManagedResource<T>, T>; };
+
+template<typename T>
+concept CustomSourceConcept = requires(T t) {
+    requires std::is_member_function_pointer_v<decltype(&T::setResourceGroup)> &&
+               std::is_base_of_v<ResourceGroupBase,
+                                 typename metrix::member_function_argument_type_list_t<
+                                   decltype(&T::setResourceGroup)>::template get_type<0>::type>;
+
+    { t.load() } -> std::same_as<std::future<void>>;
+
+    requires std::is_move_constructible_v<T>;
+};
+}
+
+#endif // CYCLONITE_RESOURCES_RESOURCE_CONCEPTS_H
---
+++ src/resources/resourceGroupBase.cpp
@@ -0,0 +1,13 @@
+//
+// Created by anton on 4/13/26.
+//
+
+#include "resourceGroupBase.h"
+
+namespace cyclonite::resources
+{
+void ResourceGroupBase::notifyResourceStateChange(ManagedResourceState newState,  std::string_view resourceName)
+{
+    // TODO::
+}
+}
