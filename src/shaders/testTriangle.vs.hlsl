b745a91dde5bd76adaba0b5cf33796c4edd8ed67
BAntDit <bantdit@gmail.com>
2026-05-02 23:11:31 +0400

    adds test shaders

---
+++ src/shaders/testTriangle.vs.hlsl
@@ -0,0 +1,27 @@
+
+struct VertexData {
+    float4 pos;
+    float2 uv;
+};
+
+static const VertexData vData[3] = {
+    {float4(0.5f, 0.5f, 0.5f, 0.5f), float2(1.f, 1.f)},
+    {float4(-0.1f, -0.5f, 0.5f, 0.5f), float2(0.f, 0.f)},
+    {float4(-0.6f, 0.2f, 0.5f, 0.5f), float2(0.f, 1.f)}
+};
+
+struct VSOutput
+{
+    float4 position : SV_Position;
+    float2 uv : TEXCOORD0;
+};
+
+VSOutput main(uint vertexId : SV_VertexID)
+{
+    VSOutput output;
+
+    output.position = vData[vertexId].pos;
+    output.uv = vData[vertexId].uv;
+
+    return output;
+}
