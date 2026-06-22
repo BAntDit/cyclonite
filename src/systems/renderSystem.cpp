596548968017bf62ee7ef5864a090e925253cbb2
BAntDit <bantdit@gmail.com>
2026-06-22 21:58:37 +0400

    adds render system

--- src/systems/renderSystem.cpp
+++ src/systems/renderSystem.cpp
@@ -4,15 +4,28 @@
 
 #include "renderSystem.h"
 #include "gfx/device.h"
+#include "gfx/queueSubmissionRecorder.h"
 #include <array>
 #include <span>
 
+#include "gfx/buffer.h"
+#include "gfx/descriptorSet.h"
+#include "gfx/pipeline.h"
+#include "gfx/renderPassBuilder.h"
+#include "material.h"
+#include "multithreading/taskManager.h"
+
 namespace cyclonite::systems {
-void Renderer::init(core::ResourceSharedRef const& deviceRef, gfx::QueueSubmissionManager* queueSubmissionManager)
+void Renderer::init(core::ResourceSharedRef const& deviceRef,
+                    core::ResourceSharedRef const& renderWindowRef,
+                    core::ResourceSharedRef const& materialRef,
+                    gfx::QueueSubmissionManager* queueSubmissionManager)
 {
     queueSubmissionManager_ = queueSubmissionManager;
-
     deviceRef_ = deviceRef;
+    renderWindowRef_ = renderWindowRef;
+    materialRef_ = materialRef;
+
     auto& device = deviceRef_.as<gfx::Device>();
 
     auto allShaderStages = gfx::ShaderStageFlagBits{ gfx::ShaderStageFlags::ALL };
@@ -20,38 +33,303 @@ void Renderer::init(core::ResourceSharedRef const& deviceRef, gfx::QueueSubmissi
     auto bindingFlags =
       gfx::BindingFlagBits{ gfx::BindingFlags::UPDATE_AFTER_BIND, gfx::BindingFlags::PARTIALLY_BOUND };
 
-    auto bindings = std::array{ gfx::Binding{ metrix::value_cast(gfx::DescriptorSpace::GLOBAL_BINDLESS),
+    auto bindings = std::array{ gfx::Binding{ metrix::value_cast(gfx::DescriptorSpace::PER_PASS),
                                               0,
                                               gfx::DescriptorType::UNIFORM_BUFFER,
-                                              100000,
-                                              allShaderStages,
-                                              setLayoutFlags,
-                                              bindingFlags },
-                                gfx::Binding{ metrix::value_cast(gfx::DescriptorSpace::GLOBAL_BINDLESS),
                                               1,
-                                              gfx::DescriptorType::STORAGE_BUFFER,
-                                              100000,
-                                              allShaderStages,
-                                              setLayoutFlags,
-                                              bindingFlags },
-                                gfx::Binding{ metrix::value_cast(gfx::DescriptorSpace::GLOBAL_BINDLESS),
-                                              2,
-                                              gfx::DescriptorType::SAMPLED_IMAGE,
-                                              100000,
-                                              allShaderStages,
-                                              setLayoutFlags,
-                                              bindingFlags },
-                                gfx::Binding{ metrix::value_cast(gfx::DescriptorSpace::GLOBAL_BINDLESS),
-                                              3,
-                                              gfx::DescriptorType::COMBINED_IMAGE_SAMPLER,
-                                              100000,
                                               allShaderStages,
                                               setLayoutFlags,
                                               bindingFlags } };
 
-    auto bindingSchema = device.getOrCreatePipelineBindingSchema(bindings, std::span<gfx::PushConstantRange>{});
+    passBindingSchemaRef_ = device.getOrCreatePipelineBindingSchema(bindings, std::span<gfx::PushConstantRange>{});
 
-    globalDescriptorSet_ = device.allocateDescriptorSetBySchema(
-      bindingSchema, metrix::value_cast(gfx::DescriptorSpace::GLOBAL_BINDLESS), false);
+    auto renderPassBuilder = cyclonite::gfx::RenderPassBuilder{};
+
+    renderPassRef_ = cyclonite::core::ResourceSharedRef{ renderPassBuilder.setDevice(deviceRef_)
+                                                           .setRenderWindow(renderWindowRef_,
+                                                                            cyclonite::gfx::Color{ 0.f, 1.f, 0.f, 1.f })
+                                                           .build() };
+}
+
+void Renderer::setupPassConstants(components::Transform const& transform, components::Camera const& camera)
+{
+    auto& device = deviceRef_.as<gfx::Device>();
+    auto [stagingRef, stagingMap] = getPassConstantStaging();
+
+    auto view = glm::inverse(transform.worldMatrix);
+    stagingMap->view = view;
+
+    auto projection = std::visit(
+      [](auto&& projection) -> mat4 {
+          if constexpr (std::is_same_v<std::decay_t<decltype(projection)>, components::Camera::PerspectiveProjection>) {
+              auto& [aspect, yFov, zNear, zFar] = projection;
+
+              real f = 1.0f / tan(0.5f * yFov);
+
+              return glm::transpose(mat4{ f / aspect,
+                                          0.f,
+                                          0.f,
+                                          0.f,
+                                          0.f,
+                                          -f,
+                                          0.f,
+                                          0.f,
+                                          0.f,
+                                          0.f,
+                                          zFar / (zNear - zFar),
+                                          -1.f,
+                                          0.f,
+                                          0.f,
+                                          (zNear * zFar) / (zNear - zFar),
+                                          0.f });
+          }
+
+          if constexpr (std::is_same_v<std::decay_t<decltype(projection)>,
+                                       components::Camera::OrthographicProjection>) {
+              auto& [xMag, yMag, zNear, zFar] = projection;
+              return glm::ortho(0.0f, xMag, 0.0f, yMag, zNear, zFar);
+          }
+          assert(false);
+      },
+      camera.projection);
+
+    stagingMap->projection = projection;
+
+    stagingMap->viewProj = glm::transpose(projection) * view;
+}
+
+auto Renderer::getPassDescriptorSet() -> core::ResourceSharedRef
+{
+    auto result = core::ResourceSharedRef();
+    auto submissionIndex = queueSubmissionManager_->currentSubmissionIndex();
+    auto& device = deviceRef_.as<gfx::Device>();
+
+    if (auto it = passDescriptorSets_.find(submissionIndex); it != passDescriptorSets_.end()) {
+        auto const& [_, ds] = *it;
+        result = ds;
+    } else {
+        auto ds = device.allocateDescriptorSetBySchema(
+          passBindingSchemaRef_, metrix::value_cast(gfx::DescriptorSpace::PER_PASS), false);
+        result = core::ResourceSharedRef{ std::move(ds) };
+
+        [[maybe_unused]] auto [itNew, success] = passDescriptorSets_.add(result, submissionIndex);
+        assert(success);
+    }
+
+    return result;
+}
+
+auto Renderer::getPassConstantStaging() -> std::pair<core::ResourceSharedRef, PassConstants*>
+{
+    auto submissionIndex = queueSubmissionManager_->currentSubmissionIndex();
+    auto& device = deviceRef_.as<gfx::Device>();
+
+    auto bufferRef = core::ResourceSharedRef();
+    auto staging = std::add_pointer_t<PassConstants>{ nullptr };
+
+    if (auto it = passConstantStagings_.find(submissionIndex); it != passConstantStagings_.end()) {
+        auto const& [_, pair] = *it;
+        bufferRef = pair.first;
+        staging = pair.second;
+    } else {
+        auto bufferNewRef = device.createBuffer(
+          gfx::GpuMemoryAllocationFlagBits{ gfx::GpuMemoryAllocationFlags::HOST_ACCESS_SEQUENTIAL_WRITE },
+          gfx::BufferUsageFlagBits{ gfx::BufferUsageFlags::TRANSFER_SRC },
+          sizeof(PassConstants));
+
+        bufferRef = core::ResourceSharedRef{ std::move(bufferNewRef) };
+        auto& buffer = bufferRef.as<gfx::Buffer>();
+
+        staging = reinterpret_cast<PassConstants*>(buffer.map());
+
+        [[maybe_unused]] auto [itNew, success] =
+          passConstantStagings_.add(std::make_pair(bufferRef, staging), submissionIndex);
+        assert(success);
+    }
+
+    return std::make_pair(bufferRef, staging);
+}
+
+auto Renderer::getPassConstantBuffer() -> core::ResourceSharedRef
+{
+    auto result = core::ResourceSharedRef();
+    auto submissionIndex = queueSubmissionManager_->currentSubmissionIndex();
+    auto& device = deviceRef_.as<gfx::Device>();
+
+    if (auto it = passConstantBuffers_.find(submissionIndex); it != passConstantBuffers_.end()) {
+        auto const& [_, ds] = *it;
+        result = ds;
+    } else {
+        auto bufferNewRef = device.createBuffer(
+          gfx::GpuMemoryAllocationFlagBits{ gfx::GpuMemoryAllocationFlags::DEDICATED_MEMORY },
+          gfx::BufferUsageFlagBits{ gfx::BufferUsageFlags::UNIFORM_BUFFER, gfx::BufferUsageFlags::TRANSFER_DST },
+          sizeof(PassConstants));
+
+        result = core::ResourceSharedRef{ std::move(bufferNewRef) };
+
+        [[maybe_unused]] auto [itNew, success] = passConstantBuffers_.add(result, submissionIndex);
+        assert(success);
+    }
+
+    return result;
+}
+
+struct PassConstantsTransferJob
+{
+    explicit PassConstantsTransferJob(Renderer* renderer)
+      : renderer_{ renderer } {};
+
+    void operator()()
+    {
+        auto* submissionManager = renderer_->queueSubmissionManager_;
+
+        renderer_->transferSubmission_ = submissionManager->acquireQueueSubmission(
+          multithreading::Purpose::Transfer, gfx::CommandPoolFlagBits{}, gfx::default_transfer_submission_priority_v);
+
+        auto& transferSubmission = renderer_->transferSubmission_.as<gfx::QueueSubmission>();
+
+        auto passConstantsBufferRef = renderer_->getPassConstantBuffer();
+        auto [passStagingRef, _] = renderer_->getPassConstantStaging();
+
+        if (!transferSubmission.isExecutable()) {
+            auto transferSubmissionRecorder = cyclonite::gfx::QueueSubmissionRecorder{ renderer_->transferSubmission_ };
+
+            transferSubmissionRecorder.start();
+
+            {
+                auto batchRecorder = transferSubmissionRecorder.addBatch("pass-constants-transfer");
+
+                auto commandListRecorder = batchRecorder.addCommandList();
+                commandListRecorder.begin(gfx::CommandListUsageFlagBits{});
+
+                // acquiring barrier
+                commandListRecorder.acquireResourceForTransfer(
+                  gfx::PipelineStageFlagBits{ gfx::PipelineStageFlags::TOP_OF_PIPE_BIT },
+                  gfx::PipelineStageFlagBits{ gfx::PipelineStageFlags::TRANSFER_BIT },
+                  gfx::AccessFlagBits{},
+                  gfx::AccessFlagBits{ gfx::AccessFlags::TRANSFER_WRITE_BIT },
+                  passConstantsBufferRef);
+
+                commandListRecorder.copyBuffers(
+                  passStagingRef, passConstantsBufferRef, 0, 0, sizeof(Renderer::PassConstants));
+
+                // release barrier
+                commandListRecorder.releaseResourceToGraphics(
+                  gfx::PipelineStageFlagBits{ gfx::PipelineStageFlags::TRANSFER_BIT },
+                  gfx::PipelineStageFlagBits{ gfx::PipelineStageFlags::BOTTOM_OF_PIPE_BIT },
+                  gfx::AccessFlagBits{ gfx::AccessFlags::TRANSFER_WRITE_BIT },
+                  gfx::AccessFlagBits{},
+                  passConstantsBufferRef);
+
+                commandListRecorder.end();
+
+                commandListRecorder.finish(); // TODO:: combine with end
+
+                batchRecorder.finish();
+            }
+
+            transferSubmissionRecorder.finish().get();
+        }
+    }
+
+    Renderer* renderer_;
+};
+
+struct PassRenderJob
+{
+    explicit PassRenderJob(Renderer* renderer)
+      : renderer_{ renderer } {};
+
+    void operator()()
+    {
+        auto* submissionManager = renderer_->queueSubmissionManager_;
+
+        auto descriptorSetRef = renderer_->getPassDescriptorSet();
+        auto& descriptorSet = descriptorSetRef.as<gfx::DescriptorSet>();
+
+        auto renderSubmissionRef = submissionManager->acquireQueueSubmission(
+          multithreading::Purpose::Render,
+          gfx::CommandPoolFlagBits{ cyclonite::gfx::CommandPoolFlags::TRANSIENT },
+          gfx::default_render_submission_priority_v);
+
+        auto& renderSubmission = renderSubmissionRef.as<gfx::QueueSubmission>();
+
+        auto renderSubmissionRecorder = cyclonite::gfx::QueueSubmissionRecorder{ renderSubmissionRef };
+
+        renderSubmissionRecorder.start();
+
+        {
+            auto batchRecorder = renderSubmissionRecorder.addBatch("test-render-pass");
+            auto commandListRecorder = batchRecorder.addCommandList();
+
+            commandListRecorder.begin(cyclonite::gfx::CommandListUsageFlagBits{});
+
+            commandListRecorder.beginRenderPass(renderer_->renderPassRef_);
+
+            auto& material = renderer_->materialRef_.as<cyclonite::Material>();
+            commandListRecorder.bindPipeline(material.pipeline());
+
+            renderer_->transferTaskFuture_.get();
+
+            auto constantBufferRef = renderer_->getPassConstantBuffer();
+
+            auto resDesc = gfx::BufferResourceDescription{};
+            resDesc.type = gfx::DescriptorType::UNIFORM_BUFFER;
+            resDesc.size = sizeof(Renderer::PassConstants);
+            resDesc.offset = 0;
+
+            auto descriptorWriteData = gfx::DescriptorWriteData{};
+            descriptorWriteData.binding = 0;
+            descriptorWriteData.desc = resDesc;
+            descriptorWriteData.element = 0;
+            descriptorWriteData.resource = constantBufferRef;
+
+            descriptorSet.update(std::span{ &descriptorWriteData, 1 });
+
+            commandListRecorder.acquireResourceForGraphics(
+              gfx::PipelineStageFlagBits{ gfx::PipelineStageFlags::TOP_OF_PIPE_BIT },
+              gfx::PipelineStageFlagBits{ gfx::PipelineStageFlags::VERTEX_SHADER_BIT,
+                                          gfx::PipelineStageFlags::FRAGMENT_SHADER_BIT },
+              gfx::AccessFlagBits{},
+              gfx::AccessFlagBits{ gfx::AccessFlags::UNIFORM_READ_BIT },
+              constantBufferRef);
+
+            auto bindingSchemaRef = material.pipeline().as<gfx::Pipeline>().bindingSchema();
+            commandListRecorder.bindDescriptorSet(gfx::PipelineBindPoint::GRAPHICS, bindingSchemaRef, descriptorSetRef);
+
+            commandListRecorder.draw(36, 1, 0, 0);
+
+            commandListRecorder.releaseResourceToTransfer(
+              gfx::PipelineStageFlagBits{ gfx::PipelineStageFlags::VERTEX_SHADER_BIT,
+                                          gfx::PipelineStageFlags::FRAGMENT_SHADER_BIT },
+              gfx::PipelineStageFlagBits{ gfx::PipelineStageFlags::BOTTOM_OF_PIPE_BIT },
+              gfx::AccessFlagBits{ gfx::AccessFlags::UNIFORM_READ_BIT },
+              gfx::AccessFlagBits{},
+              constantBufferRef);
+
+            commandListRecorder.endRenderPass();
+
+            commandListRecorder.end();
+
+            commandListRecorder.finish();
+
+            batchRecorder.finish();
+        }
+
+        renderSubmissionRecorder.finish().get();
+    }
+
+    Renderer* renderer_;
+};
+
+void Renderer::render()
+{
+    transferTaskFuture_ = multithreading::Executor::threadExecutor().taskManager().submitTask(
+      PassConstantsTransferJob{ this }, multithreading::Purpose::Transfer);
+
+    multithreading::Executor::threadExecutor()
+      .taskManager()
+      .submitTask(PassRenderJob{ this }, multithreading::Purpose::Render)
+      .get();
 }
 }
--- src/systems/renderSystem.h
+++ src/systems/renderSystem.h
@@ -5,21 +5,59 @@
 #ifndef CYCLONITE_SYSTEMS_RENDER_SYSTEM_H
 #define CYCLONITE_SYSTEMS_RENDER_SYSTEM_H
 
+#include "components/camera.h"
+#include "components/transform.h"
+#include "core/hashTable.h"
 #include "core/resourceSharedRef.h"
+#include "core/ringBuffer.h"
 #include "gfx/queueSubmissionManager.h"
 
 namespace cyclonite::systems {
+struct PassConstantsTransferJob;
+struct PassRenderJob;
+
 class Renderer
 {
+    friend struct PassConstantsTransferJob;
+    friend struct PassRenderJob;
+
+    struct PassConstants
+    {
+        mat4 view;
+        mat4 projection;
+        mat4 viewProj;
+    };
+
 public:
     Renderer() = default;
 
-    void init(core::ResourceSharedRef const& device, gfx::QueueSubmissionManager* queueSubmissionManager);
+    void init(core::ResourceSharedRef const& device,
+              core::ResourceSharedRef const& renderWindowRef,
+              core::ResourceSharedRef const& materialRef,
+              gfx::QueueSubmissionManager* queueSubmissionManager);
+
+    void setupPassConstants(components::Transform const& transform, components::Camera const& camera);
+
+    void render();
 
 private:
+    [[nodiscard]] auto getPassDescriptorSet() -> core::ResourceSharedRef;
+
+    [[nodiscard]] auto getPassConstantBuffer() -> core::ResourceSharedRef;
+
+    [[nodiscard]] auto getPassConstantStaging() -> std::pair<core::ResourceSharedRef, PassConstants*>;
+
     core::ResourceSharedRef deviceRef_;
+    core::ResourceSharedRef renderWindowRef_;
+    core::ResourceSharedRef renderPassRef_;
+    core::ResourceSharedRef materialRef_;
     gfx::QueueSubmissionManager* queueSubmissionManager_;
-    core::ResourceSharedRef globalDescriptorSet_;
+    core::ResourceSharedRef passBindingSchemaRef_;
+    core::StaticHashTable<core::ResourceSharedRef, 16, uint64_t> passDescriptorSets_;
+    core::StaticHashTable<core::ResourceSharedRef, 16, uint64_t> passConstantBuffers_;
+    core::StaticHashTable<std::pair<core::ResourceSharedRef, PassConstants*>, 16, uint64_t> passConstantStagings_;
+    std::future<void> transferTaskFuture_;
+    core::ResourceSharedRef transferSubmission_;
 };
 }
 
